5c157317752f4a4cec0aa32099e974e70a97678a
LiXueKui <269673806@qq.com>
2016-10-17 22:53:44 +0800

    hah
    打印两次

---
+++ GitDammon2/AAViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AAViewController.swift
+//  GitDammon2
+//
+//  Created by qianfeng on 16/10/17.
+//  Copyright © 2016年 LiXueKui. All rights reserved.
+//
+
+import UIKit
+
+class AAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("新建页面")
+        print("再次新建")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
